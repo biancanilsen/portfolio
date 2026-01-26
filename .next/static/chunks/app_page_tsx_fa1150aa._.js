29ee9f33429ddc56c8c67dc09b4a1bc143bdf57e
biancanilsen <biancanilsen.entra21@gmail.com>
2026-01-26 10:54:05 -0300

    add

--- .next/static/chunks/app_page_tsx_fa1150aa._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_sections_561377af._.js",
-  "static/chunks/_a73cc7b8._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_sections_SobreMim_tsx_04cc0f01._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_sections_SobreMim_tsx_3d7f3aec._.js"
-],
-    source: "dynamic"
-});
